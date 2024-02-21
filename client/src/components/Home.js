ee8c324ea06af6924b9e30a05f150c51e6c955de
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2024-02-21 14:14:34 +0200

    adding the card

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,23 @@
-
+import '../App.css'
 
 const Home = () => {
 
-    const username = "X"
+    const username = "X"       // needs to edit
 
     return (<>
-        <div className="container">
-            <nav>
+        <div className="container-fluid">
+            <nav className="nav-container">
                 <h1 className="text-center">Hello {username}</h1>
             </nav>
         </div>
+
+
+        <div className='card-details'>
+
+        </div>
+
+
+
     </>
     );
 }
